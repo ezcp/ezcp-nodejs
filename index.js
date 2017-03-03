9abb8c9c5465ac1f437658252d28b8eeb4eb5cf0
roland <roland@clanofthecloud.com>
2017-03-03 11:02:49 +0100

    new route for token

--- index.js
+++ index.js
@@ -2,12 +2,33 @@
 const package = require('./package.json')
 const fs = require("fs")
 const request = require("request")
+const os = require("os")
+const crypto = require('crypto')
+const Transform = require('stream').Transform;
+const program = require('commander')
 
-var program = require('commander')
 program
 	.version(package.version)
-	.option('-h, --host <host>', 'target hostname', process.env.EZHOST || 'https://ezcp.io')
-	.option("-s, --silent", "silent mode", false)
+	.option("-x, --passphrase <passphrase>", "encrypt / decrypt file")
+	.option("-l, --login <txid>", "register user and set a durable token")
+	.parse(process.argv)
+
+const algorithm = 'aes-256-ctr'
+var encrypt = null
+var decrypt = null
+
+if (program.passphrase) {
+	encrypt = crypto.createCipher(algorithm, program.passphrase)
+	decrypt = crypto.createDecipher(algorithm, program.passphrase)
+} else {
+	// create identity transform
+	encrypt = new Transform({ transform(chunk, encoding, callback) {callback(null, chunk) }});
+	decrypt = new Transform({ transform(chunk, encoding, callback) {callback(null, chunk) }});
+}
+
+/*
+ * core methods
+ */
 
 isStatusOK = (statuscode) => {
 	return (200 <= statuscode && statuscode < 300)
@@ -17,12 +38,21 @@ isSHA1Token = (token) => {
 	return token.match(/^[0-9a-f]{40}$/) != null
 }
 
+getDurableToken = () => {
+	try {
+		return  fs.readFileSync(os.homedir()+"/.ezcp-token")
+	} catch (err) {
+		if (err.code !== 'ENOENT') throw err;
+		return ""
+	}
+}
+
 upload = (filestream, token) => {
-	filestream.pipe(
-		request.post(`${program.host}/upload/${token}`)
+	filestream.pipe(encrypt).pipe(
+		request.post(`https://ezcp.io/upload/${token}`)
 		.on('response', (res) => {
 			if (!isStatusOK(res.statusCode)) {
-				console.error("ezcp upload status:", res.statusCode)
+				console.error("ezcp upload status:", res.statusCode, body)
 				process.exit(1)
 			}
 		})
@@ -32,14 +62,13 @@ upload = (filestream, token) => {
 		})
 	)
 	.on('error', (err) => console.error("ezcp upload:", err))
-
 }
 
 download = (filestream, token) => {
-	request.get(`${program.host}/download/${token}`)
+	request.get(`https://ezcp.io/download/${token}`)
 	.on('response', (res) => {
 		if (!isStatusOK(res.statusCode)) {
-			console.error("ezcp download status:", res.statusCode)
+			console.error("ezcp download status:", res.statusCode, body)
 			process.exit(1)
 		}
 	})
@@ -47,30 +76,68 @@ download = (filestream, token) => {
 		console.error("ezcp download:", err)
 		process.exit(1)
 	})
+	.pipe(decrypt).on('erorr' , (err)=> console.error(err))
 	.pipe(filestream)
 }
 
 gettoken = () => {
-	request.post(`${program.host}/token`, (err, res, body) => {
+	console.log(program.login)
+	request.post(`https://ezcp.io/token/${program.login}`, (err, res, body) => {
 		if (err) {
-			console.log("ezcp token:", err)
+			console.log("ezcp login:", err)
 		} else if (!isStatusOK(res.statusCode)) {
-			console.error("ezcp token status:", res.statusCode)
+			console.error("ezcp login status:", res.statusCode, body)
 		} else if (body) {
+			fs.writeFileSync(os.homedir() + "/.ezcp-token", body)
 			console.log(body)
 		} else {
-			console.error("ezcp token invalid")
+			console.error("ezcp invalid login")
 		}
 	})
 }
 
-program.parse(process.argv)
+/*
+ * command line parsing
+ */
 
 if (program.args.Command == null) {
 	if (!program.args.length) {
-		gettoken()
+		if (program.login) {
+			gettoken()
+		} else if (process.stdin.isTTY && process.stdout.isTTY) {
+			program.help()
+		} else if (process.stdin.isTTY) {
+			// download piped
+			var token = getDurableToken()
+			if (token)
+				download(process.stdout, token)
+			else 
+				console.error("You have to login first!")
+		} else if (process.stdout.isTTY) {
+			// upload piped
+			var token = getDurableToken()
+			if (token)
+				upload(process.stdin, token)
+			else
+				console.error("You have to login first!")				
+		}
 	} else if (program.args.length == 1) {
-		if (process.stdin.isTTY) {
+		var durableToken = getDurableToken()
+		if (process.stdin.isTTY && process.stdout.isTTY && durableToken) {
+			// registered user shortcut
+			const fpath = program.args[0]
+			fs.stat(fpath, (err, stats)=>{
+				if (stats && stats.isFile) {
+					// upload mode
+					const fstream = fs.createReadStream(fpath)
+					upload(fstream, durableToken)
+				} else {
+					// download mode
+					const fstream = fs.createWriteStream(fpath)
+					download(fstream, durableToken)
+				}
+			})
+		} else if (process.stdin.isTTY) {
 			const token = program.args[0]
 			if(isSHA1Token(token))
 				download(process.stdout, token)
